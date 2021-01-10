1a85599206f07d829c7bb2e9ddf7f697bd5f33b7
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2021-01-10 18:20:21 +0000

    Moved observer-only features into extra folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableBadPing } from "badping";
-import { enableLastHitOnCreepTrigger } from "lastHitOnCreep";
-import { enableBuildingCancelTrigger } from "buildingCancel";
-import { enableItemSoldTrigger } from "itemSold";
+import { enableLastHitOnCreepTrigger } from "observer_only/lastHitOnCreep";
+import { enableBuildingCancelTrigger } from "observer_only/buildingCancel";
+import { enableItemSoldTrigger } from "observer_only/itemSold";
 import { enableShowCommandsTrigger } from "showCommands";
 import { enableUnitDenyTrigger } from "unitDeny";
 import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
