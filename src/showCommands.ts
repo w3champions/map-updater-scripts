d4e250e8fb772fb8a52d0a3c9f03a505d1d4172a
Gab <gabriel@nunez.com>
2021-01-23 22:26:17 -0500

    Fixed command description for flo

--- src/showCommands.ts
+++ src/showCommands.ts
@@ -8,7 +8,7 @@ export function enableShowCommandsTrigger() {
 
     TriggerAddAction(showCommandsTrigger, () => {
         DisplayTextToPlayer(GetTriggerPlayer(), 0, 0, `\n|cff00ff00[W3C Commands]:|r\n` +
-            `  |cffffff00•|r For FLO details, type|cffffff00 -flo|r (FLO games only). \n` +
+            `  |cffffff00•|r For FLO details, type|cffffff00 !flo|r (FLO games only). \n` +
             `  |cffffff00•|r Type|cffffff00 -badping|r to cancel game. Expires after 2 minutes.\n` +
             `  |cffffff00•|r Type|cffffff00 -zoom <VALUE>|r to change your zoom level. (1650 - 3000)\n` +
             `  |cffffff00•|r Type|cffffff00 -deny|r to show/hide|cffffff00 !|r when a player's unit is denied.\n` +
