012c776ab9be0ad46d38b0d337d15e6a349ee719
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2021-01-25 01:45:46 +0000

    Added Item Buy message (for observers only) in addition to the already-existing Sell message

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableBadPing } from "badping";
 import { enableLastHitOnCreepTrigger } from "observer_only/lastHitOnCreep";
 import { enableBuildingCancelTrigger } from "observer_only/buildingCancel";
-import { enableItemSoldTrigger } from "observer_only/itemSold";
+import { enableItemSoldBoughtTrigger } from "observer_only/itemSoldBought";
 import { enableShowCommandsTrigger } from "showCommands";
 import { enableUnitDenyTrigger } from "unitDeny";
 import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
@@ -16,7 +16,7 @@ function init() {
   enableUnitDenyTrigger();
 
   // Observer-Only Features
-  enableItemSoldTrigger();
+  enableItemSoldBoughtTrigger();
   enableLastHitOnCreepTrigger();
   enableBuildingCancelTrigger();
 }
---
+++ src/observer_only/itemSoldBought.ts
@@ -0,0 +1,28 @@
+import { getPlayerRGBCode } from "utils";
+import { Trigger, Unit, Item, TextTag, MapPlayer } from "w3ts/index";
+
+export function enableItemSoldBoughtTrigger() {
+  let stackCounter: number = 0
+
+  const showMessage = (prefix: string, unit: Unit) => {
+    stackCounter = ModuloReal(stackCounter + 1, 3.00)
+    const item = Item.fromHandle(GetSoldItem())
+    const color = getPlayerRGBCode(unit.owner)
+    let tag: texttag = CreateTextTagUnitBJ(prefix + " \"" + item.name + "\"", unit.handle, -50.00 + (-50.00 * stackCounter), 10, color[0], color[1], color[2], 0)
+    SetTextTagPermanentBJ(tag, false)
+    SetTextTagVelocityBJ(tag, 30, 50.00 + (-50.00 * stackCounter))
+    SetTextTagLifespanBJ(tag, 2.50)
+    SetTextTagFadepointBJ(tag, 2.00)
+
+    // Only show if the local player is an observer
+    SetTextTagVisibility(tag, true || MapPlayer.fromLocal().isObserver())
+  }
+
+  const sellTrigger = new Trigger()
+  sellTrigger.registerAnyUnitEvent(EVENT_PLAYER_UNIT_PAWN_ITEM)
+  sellTrigger.addAction(() => { showMessage("Sold", Unit.fromHandle(GetSellingUnit())) })
+
+  const buyTrigger = new Trigger()
+  buyTrigger.registerAnyUnitEvent(EVENT_PLAYER_UNIT_SELL_ITEM)  // Not sure why it is the 'SELL_ITEM' event, but it corresponds to 'buying item from shop'
+  buyTrigger.addAction(() => { showMessage("Bought", Unit.fromHandle(GetBuyingUnit())) })
+}
