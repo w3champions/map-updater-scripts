ea25c8b207bfd41c3abbe2b630328693086b67c5
Gustav Andersson <gustav87and@gmail.com>
2023-12-18 16:59:47 +0100

    Hide save-load container to prevent saving the game

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -2,7 +2,6 @@ export function hideGameButtons() {
     let hideGameButtons = CreateTrigger();
     TriggerRegisterTimerEventSingle(hideGameButtons, 0.00);
     TriggerAddAction(hideGameButtons, () => {
-        BlzFrameSetVisible(BlzGetFrameByName("SaveGameButton", 0), false);
-        BlzFrameSetVisible(BlzGetFrameByName("LoadGameButton", 0), false);
+        BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
     });
 }
