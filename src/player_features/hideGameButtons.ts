79ebb5f68a0f92c88a1c4e1081811df4ca312546
Coff <82292207+CoffMakesThings@users.noreply.github.com>
2025-03-15 14:35:24 +0200

    Hide exit button (#32)
    * Hide and disable exit buttons - Direct users to Quit Mission
    
    * Revert alt + f4 handling

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -4,5 +4,6 @@ export function hideGameButtons() {
     TriggerAddAction(hideGameButtons, () => {
         BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
         BlzFrameSetEnable(BlzGetFrameByName("SaveGameFileEditBox" , 0), false);
+        BlzFrameSetVisible(BlzGetFrameByName("ExitButton" , 0), false);
     });
 }
