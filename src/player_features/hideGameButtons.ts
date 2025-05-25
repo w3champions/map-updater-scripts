36f7063a010efa5b9e3763c6f7c9f0a07de9676f
Coff <82292207+CoffMakesThings@users.noreply.github.com>
2025-05-25 23:41:08 +0300

    disable Exit button

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -5,5 +5,7 @@ export function hideGameButtons() {
         BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
         BlzFrameSetEnable(BlzGetFrameByName("SaveGameFileEditBox" , 0), false);
         BlzFrameSetVisible(BlzGetFrameByName("ExitButton" , 0), false);
+        BlzFrameSetEnable(BlzGetFrameByName("ConfirmQuitQuitButton" , 0), false);
+        BlzFrameSetText(BlzGetFrameByName("ConfirmQuitMessageText", 0), "Please use Quit Mission instead.");
     });
 }
