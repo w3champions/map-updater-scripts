94101a3d58db5e78105ba95da6f24ec29c01d769
Gustav Andersson <gustav87and@gmail.com>
2025-07-12 15:04:41 +0200

    Revert "Enable Exit Button for Observers (#40)"
    This reverts commit a9fa5c5fe73210fda105e61c906e6027f372954b.

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -2,12 +2,10 @@ export function hideGameButtons() {
     let hideGameButtons = CreateTrigger();
     TriggerRegisterTimerEventSingle(hideGameButtons, 0.00);
     TriggerAddAction(hideGameButtons, () => {
-        if (!IsPlayerObserver(GetLocalPlayer())) {    
-            BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
-            BlzFrameSetEnable(BlzGetFrameByName("SaveGameFileEditBox" , 0), false);
-            BlzFrameSetVisible(BlzGetFrameByName("ExitButton" , 0), false);
-            BlzFrameSetEnable(BlzGetFrameByName("ConfirmQuitQuitButton" , 0), false);
-            BlzFrameSetText(BlzGetFrameByName("ConfirmQuitMessageText", 0), "Please use Quit Mission instead.");
-        }
+        BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
+        BlzFrameSetEnable(BlzGetFrameByName("SaveGameFileEditBox" , 0), false);
+        BlzFrameSetVisible(BlzGetFrameByName("ExitButton" , 0), false);
+        BlzFrameSetEnable(BlzGetFrameByName("ConfirmQuitQuitButton" , 0), false);
+        BlzFrameSetText(BlzGetFrameByName("ConfirmQuitMessageText", 0), "Please use Quit Mission instead.");
     });
 }
