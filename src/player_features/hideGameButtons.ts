18c0585005262f379c03bc59aff2515f158a8988
Eiler Poulsen <microhive@gmail.com>
2025-07-12 14:15:24 +0200

    Enable Exit Button for Observers
    There is no reason why the Exit Button should be disabled for the observers.

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -2,10 +2,12 @@ export function hideGameButtons() {
     let hideGameButtons = CreateTrigger();
     TriggerRegisterTimerEventSingle(hideGameButtons, 0.00);
     TriggerAddAction(hideGameButtons, () => {
-        BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
-        BlzFrameSetEnable(BlzGetFrameByName("SaveGameFileEditBox" , 0), false);
-        BlzFrameSetVisible(BlzGetFrameByName("ExitButton" , 0), false);
-        BlzFrameSetEnable(BlzGetFrameByName("ConfirmQuitQuitButton" , 0), false);
-        BlzFrameSetText(BlzGetFrameByName("ConfirmQuitMessageText", 0), "Please use Quit Mission instead.");
+        if (!IsPlayerObserver(GetLocalPlayer())) {    
+            BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
+            BlzFrameSetEnable(BlzGetFrameByName("SaveGameFileEditBox" , 0), false);
+            BlzFrameSetVisible(BlzGetFrameByName("ExitButton" , 0), false);
+            BlzFrameSetEnable(BlzGetFrameByName("ConfirmQuitQuitButton" , 0), false);
+            BlzFrameSetText(BlzGetFrameByName("ConfirmQuitMessageText", 0), "Please use Quit Mission instead.");
+        }
     });
 }
