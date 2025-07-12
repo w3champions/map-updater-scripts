6b109913b20421ae3f83163783f3165f344907dd
Eiler Poulsen <microhive@gmail.com>
2025-07-12 15:54:24 +0200

    Update hideGameButtons.ts
    Resolved desync issue

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -2,12 +2,19 @@ export function hideGameButtons() {
     let hideGameButtons = CreateTrigger();
     TriggerRegisterTimerEventSingle(hideGameButtons, 0.00);
     TriggerAddAction(hideGameButtons, () => {
-        if (!IsPlayerObserver(GetLocalPlayer())) {    
-            BlzFrameSetVisible(BlzGetFrameByName("EscMenuSaveLoadContainer", 0), false);
-            BlzFrameSetEnable(BlzGetFrameByName("SaveGameFileEditBox" , 0), false);
-            BlzFrameSetVisible(BlzGetFrameByName("ExitButton" , 0), false);
-            BlzFrameSetEnable(BlzGetFrameByName("ConfirmQuitQuitButton" , 0), false);
-            BlzFrameSetText(BlzGetFrameByName("ConfirmQuitMessageText", 0), "Please use Quit Mission instead.");
-        }
+        // Get all BlzGetFrameByName handles outside of local scope to prevent async issues
+		const escMenuSaveLoadContainer: framehandle = BlzGetFrameByName('EscMenuSaveLoadContainer', 0);
+		const saveGameFileEditBox: framehandle = BlzGetFrameByName('SaveGameFileEditBox', 0);
+		const exitButton: framehandle = BlzGetFrameByName('ExitButton', 0);
+		const confirmQuitQuitButton: framehandle = BlzGetFrameByName('ConfirmQuitQuitButton', 0);
+		const confirmQuitMessageText: framehandle = BlzGetFrameByName('ConfirmQuitMessageText', 0);
+
+		if (!IsPlayerObserver(GetLocalPlayer())) {
+			BlzFrameSetVisible(escMenuSaveLoadContainer, false);
+			BlzFrameSetEnable(saveGameFileEditBox, false);
+			BlzFrameSetVisible(exitButton, false);
+			BlzFrameSetEnable(confirmQuitQuitButton, false);
+			BlzFrameSetText(confirmQuitMessageText, 'Please use Quit Mission instead.');
+		}
     });
 }
