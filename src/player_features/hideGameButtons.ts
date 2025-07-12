383b540be14f07f7821f18300207654bebfe0171
Eiler Poulsen <microhive@gmail.com>
2025-07-12 16:01:14 +0200

    Update hideGameButtons.ts
    Fixed whitespace

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -3,18 +3,18 @@ export function hideGameButtons() {
     TriggerRegisterTimerEventSingle(hideGameButtons, 0.00);
     TriggerAddAction(hideGameButtons, () => {
         // Get all BlzGetFrameByName handles outside of local scope to prevent async issues
-		const escMenuSaveLoadContainer: framehandle = BlzGetFrameByName('EscMenuSaveLoadContainer', 0);
-		const saveGameFileEditBox: framehandle = BlzGetFrameByName('SaveGameFileEditBox', 0);
-		const exitButton: framehandle = BlzGetFrameByName('ExitButton', 0);
-		const confirmQuitQuitButton: framehandle = BlzGetFrameByName('ConfirmQuitQuitButton', 0);
-		const confirmQuitMessageText: framehandle = BlzGetFrameByName('ConfirmQuitMessageText', 0);
-
-		if (!IsPlayerObserver(GetLocalPlayer())) {
-			BlzFrameSetVisible(escMenuSaveLoadContainer, false);
-			BlzFrameSetEnable(saveGameFileEditBox, false);
-			BlzFrameSetVisible(exitButton, false);
-			BlzFrameSetEnable(confirmQuitQuitButton, false);
-			BlzFrameSetText(confirmQuitMessageText, 'Please use Quit Mission instead.');
-		}
+	const escMenuSaveLoadContainer: framehandle = BlzGetFrameByName('EscMenuSaveLoadContainer', 0);
+	const saveGameFileEditBox: framehandle = BlzGetFrameByName('SaveGameFileEditBox', 0);
+	const exitButton: framehandle = BlzGetFrameByName('ExitButton', 0);
+	const confirmQuitQuitButton: framehandle = BlzGetFrameByName('ConfirmQuitQuitButton', 0);
+	const confirmQuitMessageText: framehandle = BlzGetFrameByName('ConfirmQuitMessageText', 0);
+	
+	if (!IsPlayerObserver(GetLocalPlayer())) {
+		BlzFrameSetVisible(escMenuSaveLoadContainer, false);
+		BlzFrameSetEnable(saveGameFileEditBox, false);
+		BlzFrameSetVisible(exitButton, false);
+		BlzFrameSetEnable(confirmQuitQuitButton, false);
+		BlzFrameSetText(confirmQuitMessageText, 'Please use Quit Mission instead.');
+	}
     });
 }
