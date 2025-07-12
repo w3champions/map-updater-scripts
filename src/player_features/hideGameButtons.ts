8f7cb916c790cfcdd6e7c6a0b966a1c8fabf7002
Bogdan Gligorijević <bogdangligorijevic3@gmail.com>
2025-07-12 16:09:31 +0200

    Update hideGameButtons.ts

--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -1,20 +1,20 @@
 export function hideGameButtons() {
-    let hideGameButtons = CreateTrigger();
-    TriggerRegisterTimerEventSingle(hideGameButtons, 0.00);
-    TriggerAddAction(hideGameButtons, () => {
-        // Get all BlzGetFrameByName handles outside of local scope to prevent async issues
-	const escMenuSaveLoadContainer: framehandle = BlzGetFrameByName('EscMenuSaveLoadContainer', 0);
-	const saveGameFileEditBox: framehandle = BlzGetFrameByName('SaveGameFileEditBox', 0);
-	const exitButton: framehandle = BlzGetFrameByName('ExitButton', 0);
-	const confirmQuitQuitButton: framehandle = BlzGetFrameByName('ConfirmQuitQuitButton', 0);
-	const confirmQuitMessageText: framehandle = BlzGetFrameByName('ConfirmQuitMessageText', 0);
-	
-	if (!IsPlayerObserver(GetLocalPlayer())) {
-		BlzFrameSetVisible(escMenuSaveLoadContainer, false);
-		BlzFrameSetEnable(saveGameFileEditBox, false);
-		BlzFrameSetVisible(exitButton, false);
-		BlzFrameSetEnable(confirmQuitQuitButton, false);
-		BlzFrameSetText(confirmQuitMessageText, 'Please use Quit Mission instead.');
-	}
-    });
+	let hideGameButtons = CreateTrigger();
+	TriggerRegisterTimerEventSingle(hideGameButtons, 0.00);
+	TriggerAddAction(hideGameButtons, () => {
+		// Get all BlzGetFrameByName handles outside of local scope to prevent async issues
+		const escMenuSaveLoadContainer: framehandle = BlzGetFrameByName('EscMenuSaveLoadContainer', 0);
+		const saveGameFileEditBox: framehandle = BlzGetFrameByName('SaveGameFileEditBox', 0);
+		const exitButton: framehandle = BlzGetFrameByName('ExitButton', 0);
+		const confirmQuitQuitButton: framehandle = BlzGetFrameByName('ConfirmQuitQuitButton', 0);
+		const confirmQuitMessageText: framehandle = BlzGetFrameByName('ConfirmQuitMessageText', 0);
+
+		if (!IsPlayerObserver(GetLocalPlayer())) {
+			BlzFrameSetVisible(escMenuSaveLoadContainer, false);
+			BlzFrameSetEnable(saveGameFileEditBox, false);
+			BlzFrameSetVisible(exitButton, false);
+			BlzFrameSetEnable(confirmQuitQuitButton, false);
+			BlzFrameSetText(confirmQuitMessageText, 'Please use Quit Mission instead.');
+		}
+	});
 }
