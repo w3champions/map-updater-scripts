2a5f65c62526657180c31215efcb32013c21289a
Yaroslav Buhaiev <scourgegames@gmail.com>
2025-09-23 12:15:20 +0300

    Fix "Elder Jungle Stalker" missing indicator
    "Elder Jungle Stalker" on 1v1_WarHail_v1.1.w3x.
    The data for position is not always precise, so we search in a bigger radius and pick the closest unit

--- src/player_features/loot-indicator/modules/unit-item-drops.ts
+++ src/player_features/loot-indicator/modules/unit-item-drops.ts
@@ -79,20 +79,58 @@ const RAW_UNIT_ITEM_DROPS = compiletime(() => {
 function findUnitAtPoint(p: Point): Unit | undefined {
     const g = Group.create()!;
     g.enumUnitsInRangeOfPoint(p, 1, () => true);
-    if (g.size != 1) {
-        //size=0 can happen when player spawn is at a camp (e.g., on 4 player map, like LostTemple)
+    if (g.size == 0) {
+        //Try a bigger search radius. The data for position is not always precise.
+        //Example is "Elder Jungle Stalker" on 1v1_WarHail_v1.1.w3x
+        //Parsed map position: 2338, 3121
+        //Actual     position: 2384, 3088
+        g.enumUnitsInRangeOfPoint(p, 200, () => true)
+    }
+
+    //size=0 can happen when player spawn is at a camp (e.g., on 4 player map, like LostTemple)
+    if (g.size == 0) {
+        // print(`P(${p.x}, ${p.y}) should point to 1 unit, but found ${g.size}.`)
         //TODO: remove/disable print statements in production
         // Idea: add a "-debug" command (locally persisted flag - same approach as feature flags).
         // When enabled, print statements are enabled.
         // When something goes wrong, we can ask for a replay file. Then replay it with "debug" flag enabled - and we can see what happened.
-
-        // print(`P(${p.x}, ${p.y}) should point to 1 unit, but found ${g.size}.`)
-        g.destroy()
+        g.destroy();
         return;
     }
-    const u = g.getUnitAt(0)!;
+
+    let u: Unit;
+    if(g.size == 1) {
+        u = g.getUnitAt(0);
+    } else if(g.size > 1) {
+        print(`P(${p.x}, ${p.y}) should point to 1 unit, but found ${g.size}.`)
+        u = getClosestUnit(g, p);
+    }
+
     g.destroy();
     return u;
+}
+
+function getClosestUnit(g: Group, p: Point) {
+    let closestUnit = g.getUnitAt(0);
+    let closestDist = calcDistance(closestUnit, p);
+
+    for (let i = 1; i < g.size; i++) {
+        const u = g.getUnitAt(i);
+        const dist = calcDistance(u, p)
+        if(dist < closestDist) {
+            closestDist = dist;
+            closestUnit = u;
+        }
+    }
+
+    return closestUnit;
+}
+
+function calcDistance(u: Unit, p: Point) {
+    const dx = u.x - p.x;
+    const dy = u.y - p.y;
+
+    return dx * dx + dy * dy;
 }
 
 export function getAllItemIds(dropSets: ItemDropSet[]) {
