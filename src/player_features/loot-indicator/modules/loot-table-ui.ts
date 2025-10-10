1996d19b53899a16db4b048d0e25b18ffdc6ca3c
Yaroslav Buhaiev <scourgegames@gmail.com>
2025-10-10 14:43:51 +0300

    Loot indicator improvements (#48)
    * Make loot table UI scale based on "HUD scaling" in-game option
    
    * Add all drops btn tooltip
    
    Sorts multiple drops by level

--- src/player_features/loot-indicator/modules/loot-table-ui.ts
+++ src/player_features/loot-indicator/modules/loot-table-ui.ts
@@ -1,5 +1,6 @@
 import {Frame} from "w3ts";
 import {getItemById} from "./items-db";
+import {getAllItemIds, ItemDropSet, RandomItemGroupDrop} from "./unit-item-drops";
 
 export class LootTableUI {
     static INSTANCE: LootTableUI;
@@ -7,6 +8,7 @@ export class LootTableUI {
 
     private readonly mainParent!: Frame;
     private readonly itemBtnList: ItemBtn[] = [];
+    private readonly allDropsBtn!: ItemBtn;
 
     static init() {
         LootTableUI.INSTANCE = new LootTableUI();
@@ -21,20 +23,34 @@ export class LootTableUI {
             return;
         }
 
-        this.mainParent = Frame.createType("LI_Main_Parent", Frame.fromOrigin(ORIGIN_FRAME_SIMPLE_UI_PARENT, 0)!, 0, "SIMPLEFRAME", "")!;
+        //ConsoleBottomBar scales based on "HUD scale" in-game option.
+        //It is a parent for ORIGIN_FRAME_UBERTOOLTIP and all bottom part of the UI.
+        //Parent for ConsoleBottomBar is ConsoleUI (aka ORIGIN_FRAME_SIMPLE_UI_PARENT) which does not scale.
+        let bottomUiFrame = Frame.fromName("ConsoleBottomBar", 0);
+        this.mainParent = Frame.createType("LI_Main_Parent", bottomUiFrame, 0, "SIMPLEFRAME", "")!;
+
         for (let i = 0; i < LootTableUI.MAX_ITEMS; i++) {
             const itemBtn = new ItemBtn(this.mainParent, i);
-            //ORIGIN_FRAME_COMMAND_BUTTON is available even in Replays (where commandbar is hidden by replay controls)
-            itemBtn.btn.setPoint(FRAMEPOINT_CENTER, Frame.fromOrigin(ORIGIN_FRAME_COMMAND_BUTTON, i)!, FRAMEPOINT_CENTER, 0, 0)
+            itemBtn.btn.setAllPoints(Frame.fromOrigin(ORIGIN_FRAME_COMMAND_BUTTON, i)!)
             this.itemBtnList.push(itemBtn)
         }
+
+        this.allDropsBtn = new ItemBtn(this.mainParent, LootTableUI.MAX_ITEMS)
+        this.allDropsBtn.btn.clearPoints()
+        this.allDropsBtn.btn.setSize(0.02, 0.02)
+        this.allDropsBtn.btn.setPoint(FRAMEPOINT_BOTTOMRIGHT, Frame.fromOrigin(ORIGIN_FRAME_COMMAND_BUTTON, 11), FRAMEPOINT_BOTTOMRIGHT, 0, 0)
+        this.allDropsBtn.btn.setLevel(7) //has to be above item icon
+        this.allDropsBtn.setIcon("replaceabletextures\\commandbuttons\\btnchestofgold.dds")
 
         this.hide()
     }
 
-    show(itemIds: string[]) {
+    show(dropSets: ItemDropSet[]) {
         this.mainParent.setVisible(true)
 
+        this.allDropsBtn.setTooltip(`|cffffff00Full loot table (${dropSets.length} drops)|r`, buildDropsInfoMsg(dropSets))
+
+        let itemIds = getAllItemIds(dropSets);
         // Can't fit more than 12 right now.
         if (itemIds.length > LootTableUI.MAX_ITEMS) {
             itemIds = itemIds.slice(0, LootTableUI.MAX_ITEMS)
@@ -96,7 +112,7 @@ class ItemBtn {
         this.tooltipSeparator.setPoint(FRAMEPOINT_BOTTOMRIGHT, this.tooltipDescription, FRAMEPOINT_TOPRIGHT, 0, 0.005)
 
         this.tooltipDescription.setWidth(0.285)
-        this.tooltipDescription.setAbsPoint(FRAMEPOINT_BOTTOMRIGHT, 0.79, 0.168)
+        this.tooltipDescription.setPoint(FRAMEPOINT_BOTTOMRIGHT, owner, FRAMEPOINT_BOTTOMRIGHT, -0.01, 0.168)
 
         this.tooltipBox.setPoint(FRAMEPOINT_TOPLEFT, this.tooltipTitle, FRAMEPOINT_TOPLEFT, -0.005, 0.005)
         this.tooltipBox.setPoint(FRAMEPOINT_BOTTOMRIGHT, this.tooltipDescription, FRAMEPOINT_BOTTOMRIGHT, 0.005, -0.005)
@@ -118,4 +134,50 @@ class ItemBtn {
         this.tooltipTitle.setText(title);
         this.tooltipDescription.setText(description);
     }
+}
+
+/*
+    == Drop 1 [Permanent, Level 1]
+      Slipper of Agility
+      Ring of Health
+    == Drop 2 [Custom drop pool] //List of specific items
+      Sentry Ward
+      Ring of Health
+    == Drop 3 [Custom drop pool] //Mix of specific items and GroupDrops
+      Slipper of Agility
+      Sentry Ward
+      [Permanent, Level 1]
+      - Ring of Health
+      - Sentry Ward
+      [Permanent, Level 2]
+      - Ring of Health
+ */
+function buildDropsInfoMsg(sets: ItemDropSet[]): string {
+    return sets.map((set, i) => {
+        let m = `|cffffff00== Drop ${i + 1} `;
+
+        //Short most common form
+        if (set.itemDrops.length === 1 && set.itemDrops[0] instanceof RandomItemGroupDrop) {
+            const drop = set.itemDrops[0] as RandomItemGroupDrop;
+            m += `|cff00ff00[${drop.itemGroup.itemClass}, Level ${drop.itemGroup.itemLevel}]|r\n`
+            m += set.itemDrops.flatMap(d => d.getDropItemIds())
+                .map(id => `  ${getItemById(id)!.name}`)
+                .join("\n")
+        //A set that contains a list of specific items or multiple GroupDrops, or a mix of both
+        } else {
+            m += `|cff00ff00[Custom drop pool]|r\n`
+            m += set.itemDrops.map(d => {
+                if (d instanceof RandomItemGroupDrop) {
+                    return `  |cff00ff00[${d.itemGroup.itemClass}, Level ${d.itemGroup.itemLevel}]|r\n` +
+                        d.getDropItemIds()
+                            .map(id => `  - ${getItemById(id).name}`)
+                            .join("\n")
+                } else {
+                    return `  ${getItemById(d.getRawId()).name}`
+                }
+            }).join("\n")
+        }
+
+        return m;
+    }).join("\n");
 }
