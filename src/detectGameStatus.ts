d40db543b316490568f99e44bfcf3b75161c04ca
Yaroslav Buhaiev <scourgegames@gmail.com>
2025-11-03 11:01:01 +0200

    fix: Loot indicator not visible for Observer + fix: Loot indicator shown from host perspective in replay (#55)
    * fix: Loot indicator not visible for Observer
    
    * fix: Loot indicator shown from host perspective in replay
    
    Tested with Faro, local player in replay is the host.
    Both in mine and Faro's replay local player was the host (me).

---
+++ src/detectGameStatus.ts
@@ -0,0 +1,49 @@
+export enum GameStatus {
+    ONLINE_OR_LAN,
+    OFFLINE,
+    REPLAY
+}
+
+let gameStatus: GameStatus;
+
+export function getGameStatus() {
+    if (gameStatus === null) {
+        gameStatus = detectGameStatus();
+    }
+    return gameStatus;
+}
+
+export function isReplay() {
+    return getGameStatus() === GameStatus.REPLAY;
+}
+
+// Based on https://www.hiveworkshop.com/threads/gamestatus-replay-detection.293181/
+// Note: Will remove a small radius of black fog at a player's start location - May be undesired on some custom maps
+function detectGameStatus() {
+    let i = 0;
+
+    for (; i < bj_MAX_PLAYERS; i++) {
+        if (GetPlayerController(Player(i)) === MAP_CONTROL_USER && GetPlayerSlotState(Player(i)) === PLAYER_SLOT_STATE_PLAYING) {
+            break;
+        }
+    }
+
+    const sl = GetPlayerStartLocation(Player(i));
+    const x = GetStartLocationX(sl);
+    const y = GetStartLocationY(sl);
+    // nvul is vulture, has low sight range
+    const unit = CreateUnit(Player(i), FourCC('nvul'), x, y, 0);
+    SelectUnitForPlayerSingle(unit, Player(i));
+    const isUnitSelected = IsUnitSelected(unit, Player(i));
+    RemoveUnit(unit);
+
+    if (isUnitSelected) {
+        if (ReloadGameCachesFromDisk()) {
+            return GameStatus.OFFLINE;
+        } else {
+            return GameStatus.REPLAY;
+        }
+    }
+
+    return GameStatus.ONLINE_OR_LAN;
+}
--- src/player_features/hideGameButtons.ts
+++ src/player_features/hideGameButtons.ts
@@ -1,8 +1,4 @@
-enum GameStatus {
-	ONLINE_OR_LAN,
-	OFFLINE,
-	REPLAY
-}
+import {GameStatus, getGameStatus} from "../detectGameStatus";
 
 export function hideGameButtons() {
 	let hideGameButtons = CreateTrigger();
@@ -25,35 +21,4 @@ export function hideGameButtons() {
 			BlzFrameSetText(confirmQuitMessageText, 'Please use Quit Mission instead.');
 		}
 	});
-}
-
-function getGameStatus() {
-	// Based on https://www.hiveworkshop.com/threads/gamestatus-replay-detection.293181/
-	// Note: Will remove a small radius of black fog at a player's start location - May be undesired on some custom maps
-	let i = 0;
-
-	for (; i < bj_MAX_PLAYERS; i++) {
-		if (GetPlayerController(Player(i)) === MAP_CONTROL_USER && GetPlayerSlotState(Player(i)) === PLAYER_SLOT_STATE_PLAYING) {
-			break;
-		}
-	}
-
-	const sl = GetPlayerStartLocation(Player(i));
-	const x = GetStartLocationX(sl);
-	const y = GetStartLocationY(sl);
-	// nvul is vulture, has low sight range
-	const unit = CreateUnit(Player(i), FourCC('nvul'), x, y, 0);
-	SelectUnitForPlayerSingle(unit, Player(i));
-	const isUnitSelected = IsUnitSelected(unit, Player(i));
-	RemoveUnit(unit);
-
-	if (isUnitSelected) {
-		if (ReloadGameCachesFromDisk()) {
-			return GameStatus.OFFLINE;
-		} else {
-			return GameStatus.REPLAY;
-		}
-	}
-
-	return GameStatus.ONLINE_OR_LAN;
 }
