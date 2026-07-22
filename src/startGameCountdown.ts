b9cc2d54ea333db5abfcafeaf0c56651dbc2005d
Yaroslav Buhaiev <scourgegames@gmail.com>
2026-07-22 16:32:13 +0300

    Recreate neutral stock building to restart stock delay countdown

--- src/startGameCountdown.ts
+++ src/startGameCountdown.ts
@@ -1,5 +1,6 @@
 import {Timer} from "w3ts";
 import {pauseClockW3C} from "./player_features/clock";
+import {Units} from "@objectdata/units";
 
 const COUNTDOWN_START = 5;
 const COUNTDOWN_SOUND = CreateSound("Sound\\Interface\\BattleNetTick.wav", false, false, false, 10, 10, "",);
@@ -17,8 +18,9 @@ export function enableStartGameCountdown() {
 
 function onCountdown(count: number) {
     if (count === 0) {
-        pauseGameW3C(false);
         ClearTextMessages();
+        recreateNeutralStockBuildings();
+        pauseGameW3C(false);
         return;
     }
 
@@ -30,4 +32,66 @@ function pauseGameW3C(pause: boolean) {
     PauseAllUnitsBJ(pause);
     SuspendTimeOfDay(pause);
     pauseClockW3C(pause);
+}
+
+// Recreating tavern/shop/merc resets initial stock cooldown delay for heroes/items/units in them
+function recreateNeutralStockBuildings() {
+    interface NeutralBuildingData {
+        unitTypeId: number;
+        x: number;
+        y: number;
+        facing: number;
+    }
+
+    function isNeutralStockBuilding(unitTypeId: number): boolean {
+        return unitTypeId === FourCC(Units.Tavern)
+            || unitTypeId === FourCC(Units.GoblinMerchant)
+            || unitTypeId === FourCC(Units.Marketplace)
+            || unitTypeId === FourCC(Units.GoblinLaboratory)
+            || unitTypeId === FourCC(Units.MercenaryCampAshenvale)
+            || unitTypeId === FourCC(Units.MercenaryCampBarrens)
+            || unitTypeId === FourCC(Units.MercenaryCampBlackCitadel)
+            || unitTypeId === FourCC(Units.MercenaryCampCityscape)
+            || unitTypeId === FourCC(Units.MercenaryCampDalaran)
+            || unitTypeId === FourCC(Units.MercenaryCampDungeon)
+            || unitTypeId === FourCC(Units.MercenaryCampFelwood)
+            || unitTypeId === FourCC(Units.MercenaryCampIcecrownGlacier)
+            || unitTypeId === FourCC(Units.MercenaryCampLordaeronFall)
+            || unitTypeId === FourCC(Units.MercenaryCampLordaeronSummer)
+            || unitTypeId === FourCC(Units.MercenaryCampLordaeronWinter)
+            || unitTypeId === FourCC(Units.MercenaryCampNorthrend)
+            || unitTypeId === FourCC(Units.MercenaryCampOutland)
+            || unitTypeId === FourCC(Units.MercenaryCampSunkenRuins)
+            || unitTypeId === FourCC(Units.MercenaryCampUnderground)
+            || unitTypeId === FourCC(Units.MercenaryCampVillage);
+    }
+
+    const buildings: NeutralBuildingData[] = [];
+    const group = CreateGroup();
+
+    GroupEnumUnitsOfPlayer(group, Player(PLAYER_NEUTRAL_PASSIVE), null);
+
+    ForGroup(group, () => {
+        const unit = GetEnumUnit();
+        const unitTypeId = GetUnitTypeId(unit);
+
+        if (!isNeutralStockBuilding(unitTypeId)) {
+            return;
+        }
+
+        buildings.push({
+            unitTypeId,
+            x: GetUnitX(unit),
+            y: GetUnitY(unit),
+            facing: GetUnitFacing(unit),
+        });
+
+        RemoveUnit(unit);
+    });
+
+    DestroyGroup(group);
+
+    for (const building of buildings) {
+        CreateUnit(Player(PLAYER_NEUTRAL_PASSIVE), building.unitTypeId, building.x, building.y, building.facing);
+    }
 }
